71a86cfc814608fee84891e8dd7a76c7799091b2
Seth Thompson <setheroo88@gmail.com>
2025-11-20 23:38:14 -0600

    Date display

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addFilter("dateDisplay", (dateObj) => {
+      return dateObj.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      });
+    });
+  
     return {
       dir: {
         input: "src",
